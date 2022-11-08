4155cf2436799637fa9536a14dbc405da1056baa
stalvok <80374859+stalvok@users.noreply.github.com>
2022-11-08 17:21:55 +0300

    update

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,67 +1,62 @@
 <template>
-  <div class="min-h-screen flex flex-col h-1 bg-gray"> <!--  вся ширина-->
-    <div class="container my-10 px-1 mx-auto h-full">
+  <div v-if="weather !== ''" class="min-h-screen flex flex-col bg-gray"> <!--  вся ширина-->
+    <div class="container my-6 px-1 mx-auto h-full">
       <div class="flex flex-col h-full lg:flex-row">
         <div
-          class="gap-4 bg-white h-full font-semibold flex flex-col p-2
+          class="gap-4 min-w-[336px] bg-white h-full flex flex-col p-2
           items-center rounded-t-[56px] lg:p-6 lg:rounded-l-[56px] lg:rounded-tr-none"
         >
-         <div class="h-3/4">
+         <div>
            <div class="flex items-center">
-             <img class="h-5" src="../assets/icons/search-icon.png">
+             <img class="h-5 mr-3" src="../assets/icons/search-icon.png">
              <input
                type="text"
-               class="w-full max-w-[240px] text-black outline-0 rounded-xl p-1"
+               class="w-full max-w-[240px] outline-0"
                placeholder="Search for places..."
              >
            </div>
            <img class="h-40 lg:h-56" src="../assets/icons/weather-state1.png">
-           <div class="text-6xl font-normal lg:text-8xl lg:self-start">12<sup class="text-4xl">°C</sup></div>
-           <div class="text-xl font-normal lg:text-3xl lg:self-start">Monday</div>
+           <div class="mt-36">
+             <div class="text-6xl relative font-light lg:text-7xl lg:self-start">{{kelvinToCelsius}}<span class="text-4xl relative bottom-[20px] lg:bottom-[64px]">°C</span></div>
+             <div class="text-xl font-normal lg:text-3xl lg:mt-10 lg:self-start">{{GetCurrentDay()}}</div>
+           </div>
          </div>
-
-
           <hr class="hr-line">
-          <div class="h-1/2">
-            <div class="flex flex-col w-full justify-between lg:justify-around h-full ">
-              <div class="h-12 flex items-center lg:text-2xl gap-4"><img class="inline" height="32" width="32" src="../assets/icons/rain-small.png">Rain 30%</div>
-              <div class="h-12 flex items-center lg:text-2xl gap-4"><img class="inline" height="32" width="32" src="../assets/icons/cloud-Icon.png">Mostly Cloudy</div>
-              <div class="text-2xl  lg:h-20 p-4  rounded-xl text-white z-10 bg-center bg-cover text-center relative lg:text-3xl"> <img class="h-full -m-4 rounded-xl w-full absolute -z-10" src="https://img5.goodfon.ru/original/2048x1284/4/68/nyc-skyline-new-york-in-blue.jpg"> Россия,Москва</div>
+          <div class="h-1/3 w-full">
+            <div class="flex flex-col">
+              <div>
+                <div class="h-12 capitalize flex items-center text-sm font-semibold gap-4"><img height="32" width="32" src="../assets/icons/cloud-Icon.png">{{weather.list[0].weather[0].description}}</div>
+                <div class="h-12 flex items-center text-sm font-semibold gap-4"><img height="32" width="32" src="../assets/icons/rain-small.png">Rain
+                  {{weather.list[0].main.humidity}}%</div>
+              </div>
+              <div class="text-2xl lg:mt-10 lg:h-20 p-4 rounded-xl text-white z-10 bg-center bg-cover text-center relative lg:text-3xl">
+                <img class="h-full -m-4 rounded-xl w-full absolute -z-10" src="https://img5.goodfon.ru/original/2048x1284/4/68/nyc-skyline-new-york-in-blue.jpg">
+                {{weather.city.country}},{{weather.city.name}}
+              </div>
             </div>
           </div>
-
         </div>
         <div class="flex flex-col p-2 w-full bg-content rounded-b-[56px] lg:rounded-bl-none lg:rounded-r-[56px]">
           <div class="h-64 mt-12 hidden gap-4 lg:flex flex-col lg:flex-row">
-            <div class="w-full lg:hidden rounded-xl text-gray-400 flex lg:flex-row justify-between bg-white p-1">
-              <div>День</div>
-              <div>T</div>
-              <div>Осадки</div>
-              <div>Ветер</div>
+            <div class="w-full flex flex-col justify-between bg-white h-[186px] rounded-2xl p-4 items-center">
+              <div>{{weekDays[GetCurrentDayShort() + 1]}}</div>
+              <img class="h-20" src="../assets/icons/weather-state1.png">
+              <div><span>{{weather.list[6].main.temp}} {{weather.list[6].dt_txt}}</span></div>
             </div>
             <div class="w-full flex flex-col justify-between bg-white h-[186px] rounded-2xl p-4 items-center">
-              <div>Воскресенье</div>
-              <div>9°C</div>
-              <div>20%</div>
-              <div>3 м/с</div>
+              <div>{{weekDays[GetCurrentDayShort() + 2]}}</div>
+              <img class="h-20" src="../assets/icons/weather-state1.png">
+              <div>{{weather.list[14].main.temp}} {{weather.list[14].dt_txt}}</div>
             </div>
-            <div class="w-full flex lg:flex-col justify-between bg-white p-1">
-              <div>Воскресенье</div>
-              <div>9°C</div>
-              <div>20%</div>
-              <div>3 м/с</div>
+            <div class="w-full flex flex-col justify-between bg-white h-[186px] rounded-2xl p-4 items-center">
+              <div>{{weekDays[GetCurrentDayShort() + 3]}}</div>
+              <img class="h-20" src="../assets/icons/weather-state1.png">
+              <div>{{weather.list[22].main.temp}} {{weather.list[22].dt_txt}}</div>
             </div>
-            <div class="w-full flex lg:flex-col justify-between bg-white p-1">
-              <div>Воскресенье</div>
-              <div>9°C</div>
-              <div>20%</div>
-              <div>3 м/с</div>
-            </div>
-            <div class="w-full flex lg:flex-col justify-between bg-white p-1">
-              <div>Воскресенье</div>
-              <div>9°C</div>
-              <div>20%</div>
-              <div>3 м/с</div>
+            <div class="w-full flex flex-col justify-between bg-white h-[186px] rounded-2xl p-4 items-center">
+              <div>{{weekDays[GetCurrentDayShort() + 4]}}</div>
+              <img class="h-20" src="../assets/icons/weather-state1.png">
+              <div>{{weather.list[29].main.temp}} {{weather.list[29].dt_txt}}</div>
             </div>
           </div>
           <table class="lg:hidden alight-left text-lg bg-content">
@@ -104,29 +99,70 @@
 
 <script>
 
+import weekday from 'dayjs/plugin/weekday'
+import {onMounted} from "vue";
+import dayjs from "dayjs"
 
 export default {
-  name: "MainPage"
+  name: "MainPage",
+  data () {
+    return {
+      weather: '',
+      weekDays:['Sunday','Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      userCity: '',
+
+    }
+  },
+  methods: {
+     async fetchWeather() {
+         await fetch('../src/mosk/forecast.json', {
+         headers: {
+           'Content-Type': 'application/json',
+           'Accept': 'application/json'
+         }
+       })
+           .then(res => res.json())
+           .then(json => this.weather = json)
+      console.log(this.weather,'weather')
+    },
+    GetCurrentDay() {
+       return new Date().toLocaleString("en-US", { weekday: "long" })
+    },
+    GetCurrentDayShort() {
+     return  dayjs().day()
+
+    },
+
+  },
+  computed : {
+    kelvinToCelsius() {
+      return (this.weather.list[0].main.temp - 273.15).toFixed(2).toString()
+    }
+  },
+  mounted() {
+    dayjs.extend(weekday)
+    this.fetchWeather()
+    console.log(new Date().toLocaleString("en-US", { weekday: "long" }),)
+    console.log(dayjs().day(),'!!!!!!')
+  }
 }
 
 </script>
 
 <style scoped>
 
-div {
+  div {
+  }
+  tr td {
+    padding: 10px;
+  }
+  tr:first-child td:first-child { border-top-left-radius: 10px; }
+  tr:first-child td:last-child { border-top-right-radius: 10px; }
 
-}
-tr td {
-  padding: 10px;
-}
+  tr:last-child td:first-child { border-bottom-left-radius: 10px; }
+  tr:last-child td:last-child { border-bottom-right-radius: 10px; }
 
-tr:first-child td:first-child { border-top-left-radius: 10px; }
-tr:first-child td:last-child { border-top-right-radius: 10px; }
-
-tr:last-child td:first-child { border-bottom-left-radius: 10px; }
-tr:last-child td:last-child { border-bottom-right-radius: 10px; }
-
-tr:first-child td { border-top-style: solid; }
-tr td:first-child { border-left-style: solid; }
+  tr:first-child td { border-top-style: solid; }
+  tr td:first-child { border-left-style: solid; }
 
 </style>
